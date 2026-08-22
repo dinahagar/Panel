a390a9ce10fd2aea2bc8af44069606cb7f290c29
dina-hagar <dina.hagar@sharwa.io>
2026-08-22 04:37:02 +0200

    add new product button UI

--- src/Pages/Products/products.styles.tsx
+++ src/Pages/Products/products.styles.tsx
@@ -2,17 +2,45 @@ import { Col } from "antd";
 import styled from "styled-components";
 
 export const NoProductsH1 = styled.h1`
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export const ProductsDiv = styled.div`
-    padding: 15px;
-`
+  padding: 15px;
+`;
 
 export const CardCol = styled(Col)`
-    display: flex;
-    justify-content: center;
-`
+  display: flex;
+  justify-content: center;
+`;
+
+export const ProductsHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 20px 0px 30px 0px;
+
+  @media (max-width: 400px) {
+    display: block;
+  }
+`;
+
+export const NewProductH2 = styled.h2`
+  font-size: 25px;
+`;
+
+export const NewProductButton = styled.button`
+  cursor: pointer;
+  background: #030852;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  padding: 10px 15px;
+  font-size: 14px;
+
+  @media (max-width: 400px) {
+    margin-top: 10px;
+  }
+`;
--- src/Pages/Products/products.tsx
+++ src/Pages/Products/products.tsx
@@ -1,7 +1,14 @@
 import { useGetAllProductsQuery } from "../../Services/products";
 import type { Product } from "../../Interfaces/products";
 import { useTranslation } from "react-i18next";
-import { CardCol, NoProductsH1, ProductsDiv } from "./products.styles";
+import {
+  CardCol,
+  NewProductButton,
+  NewProductH2,
+  NoProductsH1,
+  ProductsDiv,
+  ProductsHeader,
+} from "./products.styles";
 import ProductCard from "../../components/ProductCard/productCard";
 import { Row, Skeleton } from "antd";
 
@@ -10,22 +17,27 @@ const Products = () => {
   const { data: products, error, isLoading } = useGetAllProductsQuery({});
 
   return (
-    <ProductsDiv>
-      <h2>{t("Products")}</h2>
-      {error ? (
-        <NoProductsH1>{t("products_error")}</NoProductsH1>
-      ) : isLoading ? (
-        <Skeleton />
-      ) : products ? (
-        <Row>
-          {products?.map((product: Product) => (
-            <CardCol xs={24} sm={12} md={12} lg={8} xl={6} key={product?.id}>
-              <ProductCard product={product} />
-            </CardCol>
-          ))}
-        </Row>
-      ) : null}
-    </ProductsDiv>
+    <>
+      <ProductsDiv>
+        <ProductsHeader>
+          <NewProductH2>{t("Products")}</NewProductH2>
+          <NewProductButton>Add New Product</NewProductButton>
+        </ProductsHeader>
+        {error ? (
+          <NoProductsH1>{t("products_error")}</NoProductsH1>
+        ) : isLoading ? (
+          <Skeleton />
+        ) : products ? (
+          <Row>
+            {products?.map((product: Product) => (
+              <CardCol xs={24} sm={12} md={12} lg={8} xl={6} key={product?.id}>
+                <ProductCard product={product} />
+              </CardCol>
+            ))}
+          </Row>
+        ) : null}
+      </ProductsDiv>
+    </>
   );
 };
 
